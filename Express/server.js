b088f93642cbd88b9afcb68c95757800cad1dbf8
eddy <11211010@pershing.com.tw>
2025-07-30 14:39:04 +0800

    跨域

--- Express/server.js
+++ Express/server.js
@@ -1,24 +1,27 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(cors()); // 跨域問題可以在這新增來符合實際需求
 
 const responses = require('./responses');
 
 app.all("*", (req, res) => {
   const method = req.method.toLowerCase();
-  const routePath = req.path.substring(1); // 例如 "Sample/Post"
+  const routePath = req.path.substring(1); // 去除前面/ 例如 "/Sample/Post" => "Sample/Post"
 
   const response = responses[method]?.[routePath];
 
   if (response !== undefined) {
+    // 返回假資料 可根據實際情況做調整
     return res.json(response);
   }
 
   return res.status(404).json({ error: "Route not found" });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
